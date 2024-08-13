d5a1b63df5e72ac0494aa6220690a55352071acc
Messmanno <mesmercoulibaly2002@gmail.com>
2024-08-13 18:31:05 +0000

    premier commit

---
+++ src/IEducation.java
@@ -0,0 +1,12 @@
+public interface IEducation {
+
+     public String enseigner(String matiere);
+
+     public String peparerCours(String cours);
+
+     public String assisterReunion(String sujet);
+
+
+
+
+}
